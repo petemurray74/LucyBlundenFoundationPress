5078c12c45738569f0116c603310d60cf3f8db32
petemurray74 <github@thrap.co.uk>
2015-12-06 15:30:21 +0000

    testing dev branch

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
-	<!-- WP PUSHER -->
+	<!-- WP PUSHER DEV-->
 	<?php do_action( 'foundationpress_after_body' ); ?>
 
 	<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) == 'offcanvas' ) : ?>
